6fc90de2f1af80fc79d007e74b98f4224ed96f5e
ZachariahKB <zachariahakb@gmail.com>
2024-05-23 23:41:31 -0700

    pushpush

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('putDb not implemented');
+  const jateDB = await openDB ("jate", 1);
+  const tx = jateDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put ({id: 1, value: content});
+  const result = await request;
 
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => 
+  {console.error('getDb not implemented');
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate","readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
+  const result = await request;
 
+  result ? console.log(`-data got got from db`, result.value):
+  console.log(`data not found in db`)
+  return result?.value
+
+
+
+};
 initdb();
